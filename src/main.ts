d00a5ff28f1e7ff72bf85f78de7e5cb7c344c5f1
Marshall <166389852+Marshall-ED@users.noreply.github.com>
2025-04-13 16:45:23 +0800

    整合 MarkDown编辑器 & Monaco Editor代码编辑器

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import ArcoVue from "@arco-design/web-vue";
 import "@arco-design/web-vue/dist/arco.css";
 import "@/plugins/axios";
 import "@/access";
+import "bytemd/dist/index.css";
 
 createApp(App).use(ArcoVue).use(store).use(router).mount("#app");
 
---
+++ src/views/ExampleView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="exampleView">
+    {{ codeValue }}
+    <CodeEditor :value="codeValue" :handle-change="onCodeChange" />
+    <MdEditor :value="mdValue" :handle-change="onMdChange" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import MdEditor from "@/components/MdEditor.vue";
+import CodeEditor from "@/components/CodeEditor.vue"; // @ is an alias to /src
+
+const codeValue = ref();
+const mdValue = ref();
+
+const onCodeChange = (v: string) => {
+  codeValue.value = v;
+  console.log(v);
+};
+const onMdChange = (v: string) => {
+  mdValue.value = v;
+};
+</script>
